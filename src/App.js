257380fe48a77ffe434b2c010cb7ae8d275fc3c1
TaniaMitnovych <taniamitnovych15@gmail.com>
2023-04-07 02:18:30 +0300

    imported components and packages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-
+import CatMessageCard from "./components/CatMessageCard";
+import InputField from "./components/InputField";
+import UserMessageCard from "./components/UserMessageCard";
+import './index.scss';
+import { useState, useEffect, useRef } from "react";
 function App() {
+  const [message, setMessage] = useState("");
+  const [messageArray, setMessageArray] = useState([]);
+  useEffect(() => {
+    if (message !== "") {
+      setMessageArray(prev => {
+        return [...prev, <UserMessageCard message={message} />, <CatMessageCard key={new Date()} />]
+      });
+      window.scrollTo(0, document.documentElement.scrollHeight);
+    }
+  }, [message]);
   return (
     <div className="App">
-      
+      <div className="messages">
+      {
+        messageArray.map(message => message)
+      }
+      </div>
+      <InputField onButtonClick={setMessage} />
     </div>
   );
 }
